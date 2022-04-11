538cc1f4ef4256c533a63cab8d976cba411466cf
lysitay <100581960+lysitay@users.noreply.github.com>
2022-04-11 20:30:32 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,8 +1,35 @@
 // gsap.from('.navbar-brand', {opacity:0, duration:2, y:-50, ease:"bounce.out"});
 // gsap.from('.tag', {opacity:0, duration:2, x:-50, delay:1.4});
 // gsap.from('.tag', {opacity:0, duration:2, x:50, ease:'bounce.out', stagger:0.6});
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.timeline()
 .from('.navbar', {scale:0, duration:2})
 .from('.navbar-brand', {opacity:0, scale: 0, ease:"back"})
 .from('.tag', {opacity:0, y: 10, ease:"back"});
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+     trigger: '.aboutbg',
+     markers:true,
+     start:"bottom 90%", 
+     end:"top 30%",
+     scrub:3, 
+          }
+  
+});
+
+gsap.from(".about-text", {
+    duration:7,
+    y:250,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".aboutbg",
+        markers:true,
+        start:"bottom 90%", ///start at the top of our trigger and a percentage down the viewpost
+        end:"top 30%",
+          scrub:6, //4 seconds of smooting
+}
+});
+
+    
